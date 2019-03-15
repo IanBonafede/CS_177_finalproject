75e5b548c66ff61c92b64f66470dbb18eacdc703
Ian Bonafede <ibona001@ucr.edu>
2019-03-15 02:04:19 -0700

    nearly there

--- Car.cpp
+++ Car.cpp
@@ -87,9 +87,9 @@ void Car::run() {
 				( * roadZone->getRoad() )[tail].release(); // tail is in zone
 			tail++; // car length will be 2 (a small fraction of time)
 		}
-		cout << "Stopped." << "Front: " << front << " Unloading..." << endl;
+		
 	}// here we are stopped somewhere in the drop off zone
-	
+	cout << "Stopped." << "Front: " << front << " Unloading..." << endl;
 	hold(unloadTime);
 	/*
 	
